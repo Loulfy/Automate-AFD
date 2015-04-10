197e66f89e28478354c8f6d6bb824c19f32d1972
Loulfy <lorian.corbel@etu.u-bordeaux.fr>
2015-04-10 17:58:29 +0200

    main

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,19 +7,24 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 	Automate* a = new AF();
+	Automate* afd = nullptr;
 
 	AF* af = dynamic_cast<AF*>(a);
 
 	if(af != nullptr)
 	{
-		af->loadFromFile("exemple/auto-3.txt");
+		af->loadFromFile(argv[1]);
 		af->draw();
-		Automate* afd = new AFD(af);
+
+		afd = new AFD(af);
 		afd->draw();
 	}
+
+	delete af;
+	delete afd;
 	
 	return EXIT_SUCCESS;
 }
